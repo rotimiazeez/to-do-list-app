5350be6d851652965aef0b460c46b02d271b9f18
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-08-19 22:25:55 +0100

    feat: Add styling to page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import _ from 'lodash';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
-
-   return element;
- }
-
- document.body.appendChild(component());
+const toDoItems = [{ description: 'Do the chores', completed: false, index: 0 }, { description: 'Get some groceries', completed: false, index: 1 }, { description: 'Take Microverse class', completed: false, index: 2 }];
+const listWrap = document.querySelector('.todo-list-wrap');
+// IMPLEMENT TO DO ITEMS TO SCREEN
+const implementToDoItems = () => {
+  toDoItems.forEach((item) => {
+    listWrap.innerHTML += `<li class="list-item" data-key="${item.index}">
+      <div class="list-item-check-name">
+        <button type="button" class="check-item" data-key="${item.index}"></button>
+        <input class="description-input" type="text" value="${item.description}">
+      </div>
+      <button data-key="${item.index}" class="move-item"><i class="fas fa-ellipsis-v"></i></button>
+    </li>`;
+  });
+};
+implementToDoItems();
