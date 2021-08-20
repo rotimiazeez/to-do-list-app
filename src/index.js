8cd885a09dd22a928a01925e459c46a2e877da61
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-08-20 09:17:48 +0100

    feat: Create new javascript files for new functionalities

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
+/* eslint-disable import/no-cycle */
 import checkItem from './checkItem.js';
+import descriptionOnFocus from './inputsFocus.js';
 import {
   onMouseOver, onMouseOut, onDragStart, onDragEnd, onDragOver,
 } from './dragOverAndDrop.js';
+import {
+  addNewItem, clearAll, updateInput, removeItem,
+} from './addRemove.js';
 import './style.css';
 
+// SET TODO ARR
 let toDoArr = [];
-const setToDoArr = () => {
+export const setToDoArr = () => {
   if (JSON.parse(localStorage.getItem('myToDos'))) {
     toDoArr = JSON.parse(localStorage.getItem('myToDos'));
   } else {
-    toDoArr = [{ description: 'Do the chores', completed: false, index: 1 }, { description: 'Get some groceries', completed: false, index: 2 }, { description: 'Take Microverse class', completed: false, index: 3 }];
+    toDoArr = [];
   }
 };
 setToDoArr();
 const listWrap = document.querySelector('.todo-list-wrap');
 // IMPLEMENT TO DO ITEMS TO SCREEN
-const implementToDoItems = () => {
-  toDoArr.forEach((item) => {
+export const implementToDoItems = (arr) => {
+  arr.forEach((item) => {
     let isCompleted = '';
     let completedClass = '';
     if (item.completed) {
@@ -25,36 +31,20 @@ const implementToDoItems = () => {
     }
     listWrap.innerHTML += `<li class="list-item ${completedClass}" data-key="${item.index}" draggable="true">
       <div class="list-item-check-name">
- <input type="checkbox" class="check-item" data-key="${item.index}" ${isCompleted}>
-        <input class="description-input" type="text" value="${item.description}">
+        <input type="checkbox" class="check-item" data-key="${item.index}" ${isCompleted}>
+  <input class="description-input" data-key="${item.index}" type="text" value="${item.description}">
       </div>
-
- <button type="button" data-key="${item.index}" class="move-item"><i class="fas fa-ellipsis-v"></i></button>
+      <button type="button" data-key="${item.index}" class="move-item"><i class="fas fa-ellipsis-v"></i></button>
       <button type="button" data-key="${item.index}" class="remove-item"><i class="far fa-trash-alt"></i></button>
     </li>`;
   });
 };
-implementToDoItems();
+implementToDoItems(toDoArr);
 // CHECK ITEMS
 const toDoChecksInputs = [...document.querySelectorAll('.list-item .check-item')];
-toDoChecksInputs.forEach((item) => item.addEventListener('change', () => checkItem(item, toDoArr)));
+toDoChecksInputs.forEach((item) => item.addEventListener('change', () => checkItem(item)));
 // FOCUS ITEMS
 const toDoDescriptionsInputs = [...document.querySelectorAll('.list-item .description-input')];
-const descriptionOnFocus = (event, item) => {
-  if (event.type === 'focusin') {
-    item.parentElement.parentElement.classList.add('uncompleted-item');
-    item.parentElement.parentElement.style.backgroundColor = '#fffeca';
-    item.parentElement.nextElementSibling.style.display = 'none';
-    [...item.parentElement.parentElement.childNodes][5].style.display = 'flex';
-  } else if (event.type === 'focusout') {
-    item.parentElement.parentElement.classList.remove('uncompleted-item');
-    item.parentElement.parentElement.style.backgroundColor = '#fff';
-    setTimeout(() => {
-      [...item.parentElement.parentElement.childNodes][5].style.display = 'none';
-      item.parentElement.nextElementSibling.style.display = 'flex';
-    }, 1000);
-  }
-};
 toDoDescriptionsInputs.forEach((item) => item.addEventListener('focusin', (event) => descriptionOnFocus(event, item)));
 toDoDescriptionsInputs.forEach((item) => item.addEventListener('focusout', (event) => descriptionOnFocus(event, item)));
 // DRAG AND DROP FUNCTIONS
@@ -63,6 +53,17 @@ toDoItems.forEach((item) => {
   item.addEventListener('mouseover', (event) => onMouseOver(event, item));
   item.addEventListener('mouseout', (event) => onMouseOut(event, item));
   item.addEventListener('dragstart', (event) => onDragStart(event, item));
-  item.addEventListener('drop', (event) => onDragEnd(event, item, toDoArr));
+  item.addEventListener('drop', (event) => onDragEnd(event, item, toDoItems, listWrap));
 });
 listWrap.addEventListener('dragover', (event) => onDragOver(event, listWrap));
+// ADD ITEM
+const addNewButton = document.querySelector('.add-new-btn');
+addNewButton.addEventListener('click', (event) => addNewItem(event, toDoArr, toDoItems, listWrap));
+// CLEAR ALL
+const clearAllBtn = document.querySelector('.clear-all');
+clearAllBtn.addEventListener('click', () => clearAll(listWrap));
+// UPDATE ITEMS
+toDoDescriptionsInputs.forEach((item) => item.addEventListener('input', () => updateInput(item)));
+// REMOVE ITEM
+const removeBtns = document.querySelectorAll('.remove-item');
+removeBtns.forEach((item) => item.addEventListener('click', () => removeItem(item, toDoArr, listWrap)));
